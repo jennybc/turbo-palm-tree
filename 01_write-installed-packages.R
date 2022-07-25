f192961d1136fa82ae6c796fddf4d02231e32249
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-07-25 16:47:55 -0400

    Oops I guess `path` is deprecated

--- 01_write-installed-packages.R
+++ 01_write-installed-packages.R
@@ -3,6 +3,6 @@ library(tidyverse)
 ipt <- installed.packages() %>%
   as_tibble() %>%
   select(Package, LibPath, Version, Priority, Built) %>%
-  write_csv(path = "installed-packages.csv")
+  write_csv("installed-packages.csv")
 
 ipt
